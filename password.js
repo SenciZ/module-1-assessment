2bc05a388eb13e0674e325b083b89a992567724e
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-12 12:13:12 -0700

    format code

--- password.js
+++ password.js
@@ -1,6 +1,5 @@
 const readline = require("readline");
-const figlet = require('figlet');
-
+const figlet = require("figlet");
 
 const reader = readline.createInterface({
   input: process.stdin,
@@ -22,18 +21,18 @@ reader.question(
     } else if (input.length > 10 && found2 === true && found === true) {
       //Check to see if entered password is at least 10 characters and if it contains 1 or more special character AND 1 or more number
       console.log(figlet.textSync("GREAT SUCCESS!!!"));
-
-      console.log("You have succesfully validated your password and it is VERY STRONG!!!!")
-
-    } else if ((input.length > 10 && found2 === true) || found === true) {
+      console.log(
+        "You have succesfully validated your password and it is VERY STRONG!!!!"
+      );
+    } else if (input.length > 10 && found2 === true || found === true) {
       //Check to see if entered password is at least 10 characters and if it contains 1 or more special character or 1 or more number
       console.log(figlet.textSync("VERY NICE"));
-
-      console.log("You have succesfully validated your password and it is STRONG!!!");
+      console.log(
+        "You have succesfully validated your password and it is STRONG!!!"
+      );
     } else {
       //If the entered password is at least 10 characters long but does not contain any numbers or special characters
       console.log(figlet.textSync("SUCCESS"));
-
       console.log("You have succesfully validated your password!");
     }
     // This line closes the connection to the command line interface.
