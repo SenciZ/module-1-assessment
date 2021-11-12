b9ed2639676ee7147a6bacfd32e0e89ded3f3cba
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-12 10:47:29 -0700

    add regex to password condtion

--- password.js
+++ password.js
@@ -7,13 +7,17 @@ const reader = readline.createInterface({
 
 console.log("Welcome to the password validator tool!!");
 
-reader.question("Please enter the password you wish to validate", function(input){
+reader.question("Please enter the password you wish to validate: ", function(input){
 
-	
+	let regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/i;
+  let found = regex.test(input);
+  console.log(found)
 	if(input.length < 10){
         console.log("Your password is not long enough.")
+    } else if (input.length > 10 && found === true){
+        console.log("You have succesfully validated your password and it is strong!!!")
     } else {
-        console.log("You have succesfully validated your password!!!")
+      console.log("You have succesfully validated your password!")
     }
 	// This line closes the connection to the command line interface.
 	reader.close()
