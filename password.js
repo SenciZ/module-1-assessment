7ac94bcce12410ffe50c9d8b1473bd2f0234d8cc
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-12 12:14:24 -0700

    format console log lines

--- password.js
+++ password.js
@@ -21,15 +21,11 @@ reader.question(
     } else if (input.length > 10 && found2 === true && found === true) {
       //Check to see if entered password is at least 10 characters and if it contains 1 or more special character AND 1 or more number
       console.log(figlet.textSync("GREAT SUCCESS!!!"));
-      console.log(
-        "You have succesfully validated your password and it is VERY STRONG!!!!"
-      );
+      console.log("You have succesfully validated your password and it is VERY STRONG!!!!");
     } else if (input.length > 10 && found2 === true || found === true) {
       //Check to see if entered password is at least 10 characters and if it contains 1 or more special character or 1 or more number
       console.log(figlet.textSync("VERY NICE"));
-      console.log(
-        "You have succesfully validated your password and it is STRONG!!!"
-      );
+      console.log("You have succesfully validated your password and it is STRONG!!!");
     } else {
       //If the entered password is at least 10 characters long but does not contain any numbers or special characters
       console.log(figlet.textSync("SUCCESS"));
