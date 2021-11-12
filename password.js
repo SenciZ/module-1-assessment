953b50e44ee73020df055449b9bd8fb944d9aaaa
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-12 10:53:39 -0700

    add second level of strength

--- password.js
+++ password.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const reader = readline.createInterface({
   input: process.stdin,
@@ -7,18 +7,23 @@ const reader = readline.createInterface({
 
 console.log("Welcome to the password validator tool!!");
 
-reader.question("Please enter the password you wish to validate: ", function(input){
-
-	let regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/i;
-  let found = regex.test(input);
-  console.log(found)
-	if(input.length < 10){
-        console.log("Your password is not long enough.")
-    } else if (input.length > 10 && found === true){
-        console.log("You have succesfully validated your password and it is strong!!!")
+reader.question(
+  "Please enter the password you wish to validate: ",
+  function (input) {
+    let regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/i;
+    let regex2 = /[0-9]/i;
+    let found = regex.test(input);
+    let found2 = regex2.test(input);
+    if (input.length < 10) {
+      console.log("Your password is not long enough.");
+    } else if (input.length > 10 && found2 === true && found === true) {
+      console.log("You have succesfully validated your password and it is VERY STRONG!!!!");
+    } else if ((input.length > 10 && found2 === true) || found === true) {
+      console.log("You have succesfully validated your password and it is STRONG!!!");
     } else {
-      console.log("You have succesfully validated your password!")
+      console.log("You have succesfully validated your password!");
     }
-	// This line closes the connection to the command line interface.
-	reader.close()
-});
+    // This line closes the connection to the command line interface.
+    reader.close();
+  }
+);
